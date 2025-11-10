f4c2f0cad6b9af17cd794c89d793a6c4d656e3b1
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-11-10 19:47:20 +0530

    JS Fundamentals

---
+++ Day07 (JS)/script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+
+// variables
+
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+// alert(admin);   // John
+
+
+let planet = "Earth";
+let currentUser = "Alice";
+
+
+// Datatypes
+
+// let name = "Ilya";
+// console.log(typeof(`Hello ${1}`));  // Hello 1  expression in {} is a number typeof is string
+// console.log(`Hello ${"name"}`);     // Hello name
+// console.log(`Hello ${name}`);       // Hello Ilya
+// console.log(`Hello ${2}`);  // Hello 2
+
+
+// Browser Specific functions
+// let name = prompt("Enter your Name", "Aman");   // with default name
+// let name = prompt("Enter your Name");
+// console.log(`Hello ${name}`);
+// document.write(`Hello ${name}`);
+
+
+// Operators
+// let a = 1, b =1;
+// let c = ++a;
+// let d = b++;
+
+// console.log(a, b, c, d);    // 2 2 2 1
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+
+// console.log(`a ${a}`);  // 4
+// console.log(`x ${x}`);  // 5
+
+
+
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);
+
+
+
+
