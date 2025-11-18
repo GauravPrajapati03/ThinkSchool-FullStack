83824f71b9176ce7492070f4382968840aa6ed0e
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-11-18 13:08:52 +0530

    JS fundamentals Part 6 - Strings

---
+++ Day14 (JS)/script.js
@@ -0,0 +1,28 @@
+
+// let singleQuote = 'Hello';
+// let doubleQuote = "Hello";
+// let backtick = `Hello`;
+
+// console.log(singleQuote);
+// console.log(doubleQuote);
+// console.log(backtick);
+
+let name = "Gaurav"
+console.log(name.length);
+
+console.log(`Hello👋 ${name}`);
+
+// let para = "Hii I'm Gaurav";
+let para = 'Hii I\'m Gaurav';
+
+console.log(para);
+
+// Accessing characters
+console.log('name[0] ', name[0]);
+console.log('name[1] ', name[1]);
+console.log('name[0] ', name[2]);
+// or
+console.log('name.at(3) ', name.at(3));
+console.log('name.at(4) ', name.at(4));
+console.log('name.at(5) ', name.at(5));
+
